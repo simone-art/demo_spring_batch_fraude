6cf7dc5a5adad6f62813176f3a14267f8fabc9f0
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-07-05 19:42:28 -0300

    First commit

---
+++ demo-batch-fraude/src/main/java/com/example/demobatchfraude/DemoBatchFraudeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobatchfraude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBatchFraudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBatchFraudeApplication.class, args);
+	}
+
+}
---
+++ demo-batch-fraude/src/test/java/com/example/demobatchfraude/DemoBatchFraudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobatchfraude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBatchFraudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
